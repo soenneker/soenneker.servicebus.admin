576a11e0d2e237cd63644eeeafd4c6367f651877
Jake Soenneker <jake@soenneker.com>
2026-01-05 13:28:25 -0600

    Removes closure allocation

--- src/ServiceBusAdminUtil.cs
+++ src/ServiceBusAdminUtil.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus.Administration;
 using Microsoft.Extensions.Configuration;
@@ -13,32 +12,30 @@ namespace Soenneker.ServiceBus.Admin;
 ///<inheritdoc cref="IServiceBusAdminUtil"/>
 public sealed class ServiceBusAdminUtil : IServiceBusAdminUtil
 {
+    private readonly ILogger<ServiceBusAdminUtil> _logger;
+    private readonly string _connectionString;
+
     private readonly AsyncSingleton<ServiceBusAdministrationClient> _client;
 
     public ServiceBusAdminUtil(IConfiguration config, ILogger<ServiceBusAdminUtil> logger)
     {
-        _client = new AsyncSingleton<ServiceBusAdministrationClient>(() =>
-        {
-            var connectionString = config.GetValueStrict<string>("Azure:ServiceBus:ConnectionString");
+        _logger = logger;
+        _connectionString = config.GetValueStrict<string>("Azure:ServiceBus:ConnectionString");
 
-            logger.LogDebug("Initializing ServiceBus Administration Client...");
-
-            return new ServiceBusAdministrationClient(connectionString);
-        });
+        // No closure: method group
+        _client = new AsyncSingleton<ServiceBusAdministrationClient>(CreateClient);
     }
 
-    public ValueTask<ServiceBusAdministrationClient> Get(CancellationToken cancellationToken = default)
+    private ValueTask<ServiceBusAdministrationClient> CreateClient(CancellationToken token)
     {
-        return _client.Get(cancellationToken);
+        _logger.LogDebug("Initializing ServiceBus Administration Client...");
+        return ValueTask.FromResult(new ServiceBusAdministrationClient(_connectionString));
     }
 
-    public void Dispose()
-    {
-        _client.Dispose();
-    }
+    public ValueTask<ServiceBusAdministrationClient> Get(CancellationToken cancellationToken = default) =>
+        _client.Get(cancellationToken);
 
-    public ValueTask DisposeAsync()
-    {
-        return _client.DisposeAsync();
-    }
+    public void Dispose() => _client.Dispose();
+
+    public ValueTask DisposeAsync() => _client.DisposeAsync();
 }
